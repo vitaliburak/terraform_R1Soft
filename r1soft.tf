902889ea4b2cc9c4d30273f35ae200ebc0c3212b
Vitalii Burak <vvitaliy972@gmail.com>
2020-11-19 04:05:14 +0000

    r1soft

---
+++ r1soft.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "centos" {
+  ami                    = "${data.aws_ami.centos.id}"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.class2.id}"]
+  key_name               = "${aws_key_pair.class.key_name}"
+
+  root_block_device = [{
+    delete_on_termination = true
+  }]
+
+  tags = {
+    Name = "r1soft"
+  }
+}
+
+output "PIP" {
+  value = "${aws_instance.centos.public_ip}"
+}
