6075a1fa8f72513e034bdc8142a0a0f5ab37ccd4
Vitalii Burak <vvitaliy972@gmail.com>
2020-11-19 04:20:33 +0000

    updated

--- r1soft.tf
+++ r1soft.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "centos" {
   ami                    = "${data.aws_ami.centos.id}"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.class2.id}"]
+  vpc_security_group_ids = ["${aws_security_group.r1soft.id}"]
   key_name               = "${aws_key_pair.class.key_name}"
 
   root_block_device = [{
